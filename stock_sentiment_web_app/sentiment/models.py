43ab5f625a39f6ee7ec2ce5fa975ba20740250f6
Petar Tonchev <p.tonchev96@gmail.com>
2019-11-14 13:42:26 +0100

    add tweet model

--- stock_sentiment_web_app/sentiment/models.py
+++ stock_sentiment_web_app/sentiment/models.py
@@ -1,14 +1,20 @@
-from django.db import models
+from djongo import models
 
 
 class Stock(models.Model):
-    name = models.CharField()
-    ticker = models.CharField()
+    ticker = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
 
 
 class Tweet(models.Model):
+    stocks = models.EmbeddedModelField(
+        model_container=Stock
+    )
+
     text = models.TextField()
     created_at = models.DateTimeField(blank=True)
-    username = models.CharField()
-    stocks = models.ManyToManyField(Stock)
+    username = models.CharField(max_length=255)
     retweets = models.PositiveIntegerField()
+    sentiment = models.FloatField()
