9a9d73d461eb3aa450d81e8b4ee4bcacaffeebca
Mariya <maria.mladenova@windowslive.com>
2019-11-14 15:34:50 +0100

    fix url bug and change title of the website

--- stock_sentiment_web_app/sentiment/urls.py
+++ stock_sentiment_web_app/sentiment/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'sentiment'
 urlpatterns = [
     path("", views.index, name='index'),
-    path("<str:ticker>/", views.get_stock_sentiment, name='get_stock_sentiment')
+    path("sentiment/<str:ticker>/", views.get_stock_sentiment, name='get_stock_sentiment')
 ]
