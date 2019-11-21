d953369fdc6d5fd34bf446baac5d34dddcbbe8a1
Petar Tonchev <p.tonchev96@gmail.com>
2019-11-21 15:26:55 +0100

    Show the avg sentiment rounded to the second decimal

--- stock_sentiment_web_app/sentiment/views.py
+++ stock_sentiment_web_app/sentiment/views.py
@@ -3,6 +3,7 @@ from django.db.models import Avg
 from django.http import HttpResponse
 from .models import Stock, Tweet
 import time
+from .scrapers import TwitterScraper
 
 
 # Create your views here.
@@ -19,7 +20,13 @@ def get_stock_sentiment(request, stock_id):
     tweets = stock.tweet_set.all()
     sentiment = tweets.aggregate(Avg('sentiment'))
 
-    context = {'sentiment': sentiment,
+    context = {'sentiment': sentiment['sentiment__avg'],
                'stock': stock,
                'tweets': tweets}
     return render(request, 'sentiment/show_sentiment.html', context)
+
+
+def test_scraper(request):
+    scraper = TwitterScraper()
+
+    return HttpResponse(scraper.scrape_for_db_stocks())
