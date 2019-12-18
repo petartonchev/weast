9ed1eff5768b2bc920b14c43458018adaba5667e
Mariya <maria.mladenova@windowslive.com>
2019-12-18 23:24:52 +0100

    fix url of show sentiment page

--- stock_sentiment_web_app/sentiment/urls.py
+++ stock_sentiment_web_app/sentiment/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'sentiment'
 urlpatterns = [
     path("/", views.index, name='index'),
-    path("<int:stock_id>/", views.get_stock_sentiment, name='get_stock_sentiment')
+    path("/<int:stock_id>/", views.get_stock_sentiment, name='get_stock_sentiment')
 ]
